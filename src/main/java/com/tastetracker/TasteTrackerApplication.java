c5dbd06f3c3af7af5ef344f70a4886e6cf6636d7
norbx <norbi724@gmail.com>
2024-01-21 21:32:44 +0100

    Initial commit

---
+++ src/main/java/com/tastetracker/TasteTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.tastetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasteTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasteTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tastetracker/TasteTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tastetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasteTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
