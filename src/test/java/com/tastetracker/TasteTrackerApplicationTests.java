20a3a7cc6402f5eccc373289a8d1d6b04aeda1ff
norbertpilat <norbert.pilat11@gmail.com>
2024-01-23 19:46:34 +0100

    created templates: -home page, -registration, -login, -restaurant.

--- src/test/java/com/tastetracker/TasteTrackerApplicationTests.java
+++ src/test/java/com/tastetracker/TasteTrackerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TasteTrackerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
