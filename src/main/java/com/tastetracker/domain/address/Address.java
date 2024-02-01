16f09deb7831cde606b26a21a36f5f0200358b96
norbx <norbi724@gmail.com>
2024-02-01 21:32:55 +0100

    TT02

---
+++ src/main/java/com/tastetracker/domain/address/Address.java
@@ -0,0 +1,23 @@
+package com.tastetracker.domain.address;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String street;
+    private String city;
+    private String postCode;
+    private String country;
+}
