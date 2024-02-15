ce41a293c33c11386528451cbf6445774d697e33
norbertpilat <norbert.pilat11@gmail.com>
2024-02-15 14:48:30 +0100

    TT19 - Implementacja strony pojedynczej restauracji

---
+++ src/main/java/com/tastetracker/domain/restaurant/RestaurantDtoMapper.java
@@ -0,0 +1,17 @@
+package com.tastetracker.domain.restaurant;
+
+import com.tastetracker.domain.restaurant.dto.RestaurantDto;
+
+public class RestaurantDtoMapper {
+    static RestaurantDto map(Restaurant restaurant){
+        return new RestaurantDto(
+                restaurant.getId(),
+                restaurant.getName(),
+                restaurant.isPromoted(),
+                restaurant.getAddress().getStreet(),
+                restaurant.getAddress().getCity(),
+                restaurant.getAddress().getPostalCode(),
+                restaurant.getAddress().getCountry()
+        );
+    }
+}
