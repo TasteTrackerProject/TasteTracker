217b83f230510abadae47b7312ca44de42eb96b7
norbx <norbi724@gmail.com>
2024-02-21 12:27:49 +0100

    TT-29 Poprawki Css i Html

---
+++ src/main/java/com/tastetracker/web/LoginController.java
@@ -0,0 +1,14 @@
+package com.tastetracker.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController
+{
+    @GetMapping("/login")
+    public String loginForm()
+    {
+        return "login-form";
+    }
+}
