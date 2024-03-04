884327e204b9662daf79f59ca36c1e48b72dfa6d
kamilpcd <kamil.poszwa@gmail.com>
2024-03-04 09:16:13 +0100

    Dodanie możliwości pobrania szablonu z dysku

--- src/main/java/com/tastetracker/domain/email/EmailService.java
+++ src/main/java/com/tastetracker/domain/email/EmailService.java
@@ -1,33 +1,12 @@
 package com.tastetracker.domain.email;
 
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Service;
+import jakarta.mail.MessagingException;
 
-@Service
-public class EmailService
+import java.io.IOException;
+
+public interface EmailService
 {
-    private final JavaMailSender mailSender;
-
-    public EmailService( JavaMailSender mailSender )
-    {
-        this.mailSender = mailSender;
-    }
-
-    @Value( "${spring.mail.username}" )
-    private String applicationEmail;
-
-    public void sendEmail( String addressee, String subject, String message )
-    {
-        SimpleMailMessage messageToSend = new SimpleMailMessage();
-        messageToSend.setFrom( applicationEmail ) ;
-        messageToSend.setTo( addressee );
-        messageToSend.setText( message );
-        messageToSend.setSubject( subject );
-
-
-        mailSender.send( messageToSend );
-    }
+    public void sendEmail( String addressee, String subject, String message );
+    public void sendDynamicEmail( String addressee, String subject, String templatePath, String dynamicText )
+        throws MessagingException, IOException;
 }
