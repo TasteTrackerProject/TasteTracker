001fda15f5f0d290dd63df0c16613bdd87ad91b9
kamilpcd <kamil.poszwa@gmail.com>
2024-03-04 09:16:13 +0100

    Dodanie możliwości pobrania szablonu z dysku

--- src/main/java/com/tastetracker/web/listener/RegistrationListener.java
+++ src/main/java/com/tastetracker/web/listener/RegistrationListener.java
@@ -1,25 +1,35 @@
 package com.tastetracker.web.listener;
 
-import com.tastetracker.domain.email.EmailService;
+import com.tastetracker.domain.email.EmailServiceImpl;
 import com.tastetracker.domain.user.User;
 import com.tastetracker.domain.user.UserService;
 import com.tastetracker.event.OnRegistrationCompleteEvent;
+import jakarta.mail.MessagingException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import java.io.IOException;
 import java.util.UUID;
 
 @Component
-@AllArgsConstructor
 public class RegistrationListener implements ApplicationListener<OnRegistrationCompleteEvent>
 {
 
-    private final EmailService emailService;
+    private final EmailServiceImpl emailService;
     private final UserService userService;
+    @Value( "${spring.accountactivation.template}" )
+    private String templateToConfirmationEmailPath;
+
+    public RegistrationListener( EmailServiceImpl emailService, UserService userService )
+    {
+        this.emailService = emailService;
+        this.userService = userService;
+    }
 
     @Override
     public void onApplicationEvent( OnRegistrationCompleteEvent event )
@@ -34,19 +44,23 @@ public class RegistrationListener implements ApplicationListener<OnRegistrationC
         User user = event.getUser();
         String token = UUID.randomUUID().toString();
 
-        userService.createVeryficationToken( user, token );
+        userService.saveVeryficationToken( user, token );
 
         String recipientAddress = user.getEmail();
         String subject = "Potwierdzenie rejestracji";
 
         String appBaseUrl = getAppBaseUrl();
-
         String confirmationUrl = appBaseUrl + "/account-activation?token=" + token;
 
-        String message = "Cześć, bardzo nam miło, że chcesz dołączyć do naszej społeczności. Potwierdź aktywację konta poprzez wejście w link: "
-            + confirmationUrl;
+        try
+        {
+            emailService.sendDynamicEmail( recipientAddress, subject, templateToConfirmationEmailPath, confirmationUrl );
+        }
+        catch ( MessagingException | IOException e )
+        {
+            throw new RuntimeException( e );
+        }
 
-        emailService.sendEmail( recipientAddress, subject, message );
     }
 
     private static String getAppBaseUrl()
